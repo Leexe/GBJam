87138035002f668c3df4aad3ee2f9dd35d05c3ce
Shadow <unkwndev5@gmail.com>
2026-09-16 19:56:01 +0600

    Squashed commit of the following:
    commit 06374151bd28897a6036bc9a7fbc14508a602e09
    Author: Shadow <unkwndev5@gmail.com>
    Date:   Wed Sep 16 19:55:13 2026 +0600
    
        menu finished
    
    commit d65c866cec3a53abe96aaf373898b0f40e1fb24c
    Author: Shadow <unkwndev5@gmail.com>
    Date:   Wed Sep 16 19:36:28 2026 +0600
    
        tweak
    
    commit c0f80892bec6595568e162fbbcb108d0556df49a
    Author: Shadow <unkwndev5@gmail.com>
    Date:   Tue Sep 15 15:19:30 2026 +0600
    
        wip
    
    commit 53f3445c776888082c209d3e9ae53512fe0cf786
    Author: Shadow <unkwndev5@gmail.com>
    Date:   Tue Sep 15 14:22:20 2026 +0600
    
        menu 1.0

---
+++ Assets/Scripts/UI/Menu/MainMenuController.cs
@@ -0,0 +1,320 @@
+using PrimeTween;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+	private enum MenuState
+	{
+		Splash,
+		Main,
+		Settings,
+		Credits,
+	}
+
+	[Header("Canvas & Fade")]
+	[SerializeField] private Canvas _canvas;
+	[SerializeField] private CanvasGroup _fadeOverlay;
+
+	[Header("Panels")]
+	[SerializeField] private GameObject _splashPanel;
+	[SerializeField] private GameObject _mainMenuPanel;
+	[SerializeField] private GameObject _settingsPanel;
+	[SerializeField] private GameObject _creditsPanel;
+
+	[Header("Sub Controllers")]
+	[SerializeField] private SettingsMenuController _settingsController;
+	[SerializeField] private CreditsMenuController _creditsController;
+
+	[Header("Menu Items")]
+	[SerializeField] private RectTransform[] _menuItems;
+	[SerializeField] private RectTransform _mainCursor;
+	[SerializeField] private float _cursorXOffset = -8f;
+
+	[Header("Settings")]
+	[SerializeField] private float _splashDuration = 1.8f;
+	[SerializeField] private string _gameplaySceneName = "Main";
+
+	private MenuState _currentState = MenuState.Splash;
+	private int _currentMenuItem;
+	private float _splashTimer;
+	private bool _isTransitioning;
+	private bool _isSubscribed;
+	private Sequence _cursorTween;
+	private Vector2Int _lastDirection;
+	private float _holdTimer;
+
+	private void Awake()
+	{
+		if (_canvas == null)
+		{
+			_canvas = GetComponent<Canvas>();
+		}
+		if (_canvas != null && _canvas.worldCamera == null)
+		{
+			_canvas.worldCamera = Camera.main;
+		}
+	}
+
+	private void OnEnable()
+	{
+		if (InputManager.Instance != null)
+		{
+			SubscribeInput();
+		}
+		if (_settingsController != null) _settingsController.OnBackRequested += ReturnToMainMenu;
+		if (_creditsController != null) _creditsController.OnBackRequested += ReturnToMainMenu;
+	}
+
+	private void OnDisable()
+	{
+		UnsubscribeInput();
+		if (_settingsController != null) _settingsController.OnBackRequested -= ReturnToMainMenu;
+		if (_creditsController != null) _creditsController.OnBackRequested -= ReturnToMainMenu;
+		_cursorTween.Stop();
+	}
+
+	private void Start()
+	{
+		if (!_isSubscribed)
+		{
+			SubscribeInput();
+		}
+		SetState(MenuState.Splash);
+
+		if (_fadeOverlay != null)
+		{
+			_fadeOverlay.alpha = 1f;
+			Tween.Alpha(_fadeOverlay, 0f, 0.25f, useUnscaledTime: true);
+		}
+	}
+
+	private void Update()
+	{
+		if (_currentState == MenuState.Splash)
+		{
+			_splashTimer += Time.deltaTime;
+			if (_splashTimer >= _splashDuration)
+			{
+				SetState(MenuState.Main);
+			}
+		}
+	}
+
+	private void SubscribeInput()
+	{
+		if (_isSubscribed || InputManager.Instance == null) return;
+
+		InputManager.Instance.OnMovement += HandleMovement;
+		InputManager.Instance.OnConfirm += HandleConfirm;
+		InputManager.Instance.OnCancel += HandleCancel;
+		InputManager.Instance.OnStart += HandleConfirm;
+		_isSubscribed = true;
+	}
+
+	private void UnsubscribeInput()
+	{
+		if (!_isSubscribed || InputManager.Instance == null) return;
+
+		InputManager.Instance.OnMovement -= HandleMovement;
+		InputManager.Instance.OnConfirm -= HandleConfirm;
+		InputManager.Instance.OnCancel -= HandleCancel;
+		InputManager.Instance.OnStart -= HandleConfirm;
+		_isSubscribed = false;
+	}
+
+	private void HandleMovement(Vector2 direction)
+	{
+		if (_isTransitioning) return;
+
+		Vector2Int dir = Vector2Int.zero;
+		if (Mathf.Abs(direction.y) >= 0.5f) dir.y = direction.y > 0 ? 1 : -1;
+		else if (Mathf.Abs(direction.x) >= 0.5f) dir.x = direction.x > 0 ? 1 : -1;
+
+		if (dir == Vector2Int.zero)
+		{
+			_lastDirection = Vector2Int.zero;
+			_holdTimer = 0f;
+			return;
+		}
+
+		if (_currentState == MenuState.Splash)
+		{
+			SetState(MenuState.Main);
+			return;
+		}
+
+		if (dir != _lastDirection)
+		{
+			_lastDirection = dir;
+			_holdTimer = 0.25f;
+			NavigateDirection(dir);
+		}
+		else
+		{
+			_holdTimer -= Time.unscaledDeltaTime;
+			if (_holdTimer <= 0f)
+			{
+				_holdTimer = 0.12f;
+				NavigateDirection(dir);
+			}
+		}
+	}
+
+	private void NavigateDirection(Vector2Int dir)
+	{
+		if (_currentState == MenuState.Settings && _settingsController != null)
+		{
+			_settingsController.HandleNavigation(dir);
+		}
+		else if (_currentState == MenuState.Main && dir.y != 0 && _menuItems != null && _menuItems.Length > 0)
+		{
+			int step = dir.y > 0 ? -1 : 1;
+			_currentMenuItem = (_currentMenuItem + step + _menuItems.Length) % _menuItems.Length;
+			UpdateCursorPosition();
+		}
+	}
+
+	private void HandleConfirm()
+	{
+		if (_isTransitioning) return;
+
+		if (_currentState == MenuState.Splash)
+		{
+			SetState(MenuState.Main);
+			return;
+		}
+
+		switch (_currentState)
+		{
+			case MenuState.Main:
+				ExecuteMenuItem(_currentMenuItem);
+				break;
+			case MenuState.Settings:
+				if (_settingsController != null) _settingsController.HandleConfirm();
+				break;
+			case MenuState.Credits:
+				if (_creditsController != null) _creditsController.HandleConfirm();
+				break;
+		}
+	}
+
+	private void HandleCancel()
+	{
+		if (_isTransitioning) return;
+
+		if (_currentState == MenuState.Splash)
+		{
+			SetState(MenuState.Main);
+			return;
+		}
+
+		switch (_currentState)
+		{
+			case MenuState.Settings:
+				if (_settingsController != null) _settingsController.HandleCancel();
+				break;
+			case MenuState.Credits:
+				if (_creditsController != null) _creditsController.HandleCancel();
+				break;
+		}
+	}
+
+	private void ExecuteMenuItem(int index)
+	{
+		switch (index)
+		{
+			case 0:
+				PlayGame();
+				break;
+			case 1:
+				SetState(MenuState.Settings);
+				break;
+			case 2:
+				SetState(MenuState.Credits);
+				break;
+			case 3:
+				QuitApplication();
+				break;
+		}
+	}
+
+	private void PlayGame()
+	{
+		_isTransitioning = true;
+		if (_fadeOverlay != null)
+		{
+			Tween.Alpha(_fadeOverlay, 1f, 0.25f, useUnscaledTime: true)
+				.OnComplete(() => SceneManager.LoadScene(_gameplaySceneName));
+		}
+		else
+		{
+			SceneManager.LoadScene(_gameplaySceneName);
+		}
+	}
+
+	private void ReturnToMainMenu() => SetState(MenuState.Main);
+
+	private void SetState(MenuState newState)
+	{
+		_currentState = newState;
+
+		if (_splashPanel != null) _splashPanel.SetActive(newState == MenuState.Splash);
+		if (_mainMenuPanel != null) _mainMenuPanel.SetActive(newState == MenuState.Main);
+
+		if (_settingsPanel != null)
+		{
+			_settingsPanel.SetActive(newState == MenuState.Settings);
+			if (_settingsController != null)
+			{
+				if (newState == MenuState.Settings) _settingsController.Open();
+				else _settingsController.Close();
+			}
+		}
+
+		if (_creditsPanel != null)
+		{
+			_creditsPanel.SetActive(newState == MenuState.Credits);
+			if (_creditsController != null)
+			{
+				if (newState == MenuState.Credits) _creditsController.Open();
+				else _creditsController.Close();
+			}
+		}
+
+		if (newState == MenuState.Main)
+		{
+			UpdateCursorPosition();
+		}
+		else
+		{
+			_cursorTween.Stop();
+		}
+	}
+
+	private void UpdateCursorPosition()
+	{
+		if (_mainCursor == null || _menuItems == null || _menuItems.Length == 0) return;
+
+		RectTransform target = _menuItems[_currentMenuItem];
+		if (target == null) return;
+
+		_cursorTween.Stop();
+		float leftEdge = target.anchoredPosition.x - (target.rect.width * target.pivot.x);
+		_mainCursor.anchoredPosition = new Vector2(leftEdge + _cursorXOffset, target.anchoredPosition.y);
+
+		float originX = _mainCursor.anchoredPosition.x;
+		_cursorTween = Sequence.Create(-1, Sequence.SequenceCycleMode.Yoyo, useUnscaledTime: true)
+			.Chain(Tween.UIAnchoredPositionX(_mainCursor, originX - 2f, 0.35f, Ease.InOutSine))
+			.Chain(Tween.UIAnchoredPositionX(_mainCursor, originX, 0.35f, Ease.InOutSine));
+	}
+
+	private void QuitApplication()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+}

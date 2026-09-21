5a651f6bc88ec4dea3d688829fdf07fcfc8f98d5
Tuan Le <tuanle9333@gmail.com>
2026-09-20 20:14:05 -0700

    Added FMOD

--- Assets/Scripts/Managers/AudioManager/FMODEvents.cs
+++ Assets/Scripts/Managers/AudioManager/FMODEvents.cs
@@ -9,33 +9,93 @@ public class FMODEvents : MonoSingleton<FMODEvents>
 {
 	#region Music
 
-	// [field: SerializeField]
-	// [field: FoldoutGroup("Music", true)]
-	// public EventReference FightingGame_Bgm { get; private set; }
+	[field: SerializeField]
+	[field: FoldoutGroup("Music", true)]
+	public EventReference Gameplay1_Bgm { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("Music")]
+	public EventReference Gameplay2_Bgm { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("Music")]
+	public EventReference Gameplay3_Bgm { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("Music")]
+	public EventReference Storytime_Bgm { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("Music")]
+	public EventReference Title_Bgm { get; private set; }
 
 	#endregion
 
 	#region Ambience
 
-	// [field: SerializeField]
-	// [field: FoldoutGroup("Ambience", expanded: true)]
-	// public EventReference Ambience_Amb { get; private set; }
+	[field: SerializeField]
+	[field: FoldoutGroup("Ambience", true)]
+	public EventReference TheSeaIsReallyMad_Amb { get; private set; }
 
 	#endregion
 
 	#region SFX
 
-	// [field: SerializeField]
-	// [field: FoldoutGroup("SFX")]
-	// public EventReference Jump_Sfx { get; private set; }
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/Combat", true)]
+	public EventReference Balista_Sfx { get; private set; }
 
-	#endregion
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/Combat")]
+	public EventReference Cannon_Sfx { get; private set; }
 
-	#region Looping SFX
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/Combat")]
+	public EventReference Gun_Sfx { get; private set; }
 
-	// [field: SerializeField]
-	// [field: FoldoutGroup("Loop SFX", true)]
-	// public EventReference Lose_LoopSFX { get; private set; }
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/Combat")]
+	public EventReference Melee_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/Creature Enters", true)]
+	public EventReference CreatureEnters_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/Kills", true)]
+	public EventReference CrackyKill_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/Kills")]
+	public EventReference SquishKill_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/Mechanics", true)]
+	public EventReference PickUp_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/Mechanics")]
+	public EventReference Settle_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/UI", true)]
+	public EventReference CantClick_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/UI")]
+	public EventReference CompleteClick_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/UI")]
+	public EventReference SelectorClick_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/YouWin YouLose", true)]
+	public EventReference LosingJingle_Sfx { get; private set; }
+
+	[field: SerializeField]
+	[field: FoldoutGroup("SFX/YouWin YouLose")]
+	public EventReference WinningJingle_Sfx { get; private set; }
 
 	#endregion
 }
